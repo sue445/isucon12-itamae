18a4f5002a363c56716dc70906691343978b4dea
sue445 <sue445@sue445.net>
2021-08-19 22:05:12 +0900

    Skip ruby install when ruby.version isn't found

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -4,10 +4,12 @@ git node[:xbuild][:path] do
 end
 
 # xbuildで最新のrubyを入れる
-execute "#{node[:xbuild][:path]}/ruby-install #{node[:ruby][:version]} #{home_dir}/local/ruby" do
-  user "isucon"
+if node.dig(:ruby, :version)
+  execute "#{node[:xbuild][:path]}/ruby-install #{node[:ruby][:version]} #{home_dir}/local/ruby" do
+    user "isucon"
 
-  not_if "#{node[:ruby][:binary]} --version | grep #{node[:ruby][:version]}"
+    not_if "#{node[:ruby][:binary]} --version | grep #{node[:ruby][:version]}"
+  end
 end
 
 node[:gem][:install].each do |name|
