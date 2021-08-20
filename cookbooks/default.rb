e48a1444b7cc2ee9c0b54521a4e3e18240dd5359
sue445 <sue445@sue445.net>
2021-08-20 11:06:44 +0900

    Add bench

--- cookbooks/default.rb
+++ cookbooks/default.rb
@@ -2,5 +2,8 @@ include_recipe "./system"
 include_recipe "./mysql"
 include_recipe "./user"
 
-# NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
-include_recipe "./ruby"
+# NOTE: ベンチマークサーバではrubyは不要
+unless node[:hostname].include?("bench")
+  # NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
+  include_recipe "./ruby"
+end
