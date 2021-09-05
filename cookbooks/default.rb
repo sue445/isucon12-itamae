45eb0ebfdf64f79fb2e0c1204cff443c9c1a7728
GO Sueyoshi <sue445@users.noreply.github.com>
2021-09-06 01:03:02 +0900

    Tweak order

--- cookbooks/default.rb
+++ cookbooks/default.rb
@@ -2,12 +2,12 @@ include_recipe "./system"
 include_recipe "./mysql"
 include_recipe "./user"
 
+if node[:newrelic]
+  include_recipe "./newrelic"
+end
+
 # NOTE: ベンチマークサーバではrubyは不要
 unless node[:hostname].include?("bench")
   # NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
   include_recipe "./ruby"
-end
-
-if node[:newrelic]
-  include_recipe "./newrelic"
 end
