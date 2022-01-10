21f2f069252bf96be8a57b38fde225df12a06c86
sue445 <sue445@sue445.net>
2022-01-10 17:00:30 +0900

    Ignore newrelic and datadog on bench

--- cookbooks/default.rb
+++ cookbooks/default.rb
@@ -36,14 +36,15 @@ end
 include_recipe "./system"
 include_recipe "./user"
 include_recipe "./mysql"
-include_recipe "./datadog"
 
-if node[:newrelic]
-  include_recipe "./newrelic"
-end
-
-# NOTE: ベンチマークサーバではrubyは不要
+# NOTE: ベンチマークサーバでは不要
 unless node[:hostname].include?("bench")
+  include_recipe "./datadog"
+
+  if node[:newrelic]
+    include_recipe "./newrelic"
+  end
+
   # NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
   include_recipe "./ruby"
 end
