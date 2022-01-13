f4aa6deaf8515a4c271068f22fc6f1ddb38ac356
sue445 <sue445@sue445.net>
2022-01-13 22:54:10 +0900

    Install td-agent

---
+++ cookbooks/fluentd/install-td-agent.rb
@@ -0,0 +1,18 @@
+case node[:platform]
+when "ubuntu"
+  # c.f. https://docs.fluentd.org/installation/install-by-deb
+  if node[:platform_version] >= '20.04'
+    # Focal
+    execute "curl -fsSL https://toolbelt.treasuredata.com/sh/install-ubuntu-focal-td-agent4.sh | sh" do
+      not_if "systemctl list-unit-files --type=service | grep td-agent"
+    end
+  else
+    raise NotImplementedError, "Platform version'#{node[:platform_version]}' is not supported by fluentd/td-agent yet"
+  end
+else
+  raise NotImplementedError, "Platform '#{node[:platform]}' is not supported by fluentd/td-agent yet"
+end
+
+service "td-agent" do
+  action [:start, :enable]
+end
