f3df194597ed7d9c74c7425e4c9386ea7e234755
sue445 <sue445@sue445.net>
2022-01-13 23:23:06 +0900

    Install fluent-plugin-dogstatsd

--- cookbooks/fluentd/default.rb
+++ cookbooks/fluentd/default.rb
@@ -1,5 +1,14 @@
 include_recipe "./install-td-agent"
 
+%w(
+  fluent-plugin-dogstatsd
+).each do |name|
+  gem_package name do
+    gem_binary "td-agent-gem"
+    options %w(--no-doc)
+  end
+end
+
 template "/etc/td-agent/td-agent.conf" do
   owner "root"
   group "root"
