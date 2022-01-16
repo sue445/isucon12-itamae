38d5447d59a70bd17204ec61ea268f021c82e3e8
sue445 <sue445@sue445.net>
2022-01-16 12:41:39 +0900

    Use template instead of file edit

--- cookbooks/datadog/default.rb
+++ cookbooks/datadog/default.rb
@@ -8,60 +8,6 @@ service "datadog-agent" do
   action :nothing
 end
 
-file "/etc/datadog-agent/datadog.yaml" do
-  mode "644"
-
-  only_if "ls /etc/datadog-agent/datadog.yaml"
-end
-
-file "/etc/datadog-agent/datadog.yaml" do
-  action :edit
-
-  block do |content|
-    tags_yaml = <<~YAML
-      tags:
-        - service:isucon
-
-    YAML
-
-    if content.match?(/^tags:$/)
-      # tagsがあれば書き換える
-      content.gsub!(/^tags:\n.+\n\n/m, tags_yaml)
-    else
-      # tagsがなければ末尾に追加する
-      content << tags_yaml
-    end
-
-    logs_enabled_yaml = <<~YAML
-      logs_enabled: true
-    YAML
-
-    if content.match?(/^logs_enabled:/)
-      # logs_enabledがあれば書き換える
-      content.gsub!(/^logs_enabled: .+$/, logs_enabled_yaml.strip)
-    else
-      # logs_enabledがなければ末尾に追加する
-      content << logs_enabled_yaml
-    end
-
-    use_dogstatsd_yaml = <<~YAML
-      use_dogstatsd: true
-    YAML
-
-    if content.match?(/^use_dogstatsd:/)
-      # use_dogstatsdがあれば書き換える
-      content.gsub!(/^use_dogstatsd: .+$/, use_dogstatsd_yaml.strip)
-    else
-      # use_dogstatsdがなければ末尾に追加する
-      content << use_dogstatsd_yaml
-    end
-  end
-
-  only_if "ls /etc/datadog-agent/datadog.yaml"
-
-  notifies :restart, "service[datadog-agent]"
-end
-
 directory "/etc/datadog-agent/" do
   mode "755"
 end
