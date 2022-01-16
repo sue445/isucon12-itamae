e992f73067c1d54713df5b4c50139f70f0fd4e80
sue445 <sue445@sue445.net>
2022-01-16 15:14:33 +0900

    Tweak bench

--- cookbooks/default.rb
+++ cookbooks/default.rb
@@ -1,18 +1,18 @@
 include_recipe "./initialize"
 include_recipe "./system"
 include_recipe "./user"
+
+# NOTE: ベンチマークサーバでは以降は不要
+return if node[:hostname].include?("bench")
+
 include_recipe "./mysql"
 include_recipe "./nginx"
 include_recipe "./fluentd"
+include_recipe "./datadog"
 
-# NOTE: ベンチマークサーバでは不要
-unless node[:hostname].include?("bench")
-  include_recipe "./datadog"
-
-  if node[:newrelic]
-    include_recipe "./newrelic"
-  end
-
-  # NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
-  include_recipe "./ruby"
+if node[:newrelic]
+  include_recipe "./newrelic"
 end
+
+# NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
+include_recipe "./ruby"
