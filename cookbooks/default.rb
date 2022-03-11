b26873257e665c24d4d82ce0888611aefe3ef9c9
sue445 <sue445@sue445.net>
2022-03-12 01:02:43 +0900

    Remove newrelic cookbook

--- cookbooks/default.rb
+++ cookbooks/default.rb
@@ -10,9 +10,5 @@ include_recipe "./nginx"
 include_recipe "./fluentd"
 include_recipe "./datadog"
 
-if node[:newrelic]
-  include_recipe "./newrelic"
-end
-
 # NOTE: これが一番重いので他の作業が中断しないように一番最後に実行する
 include_recipe "./ruby"
