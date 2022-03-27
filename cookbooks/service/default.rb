81f54ca7b958960929f4f505107d5c81596628d4
sue445 <sue445@sue445.net>
2022-03-27 15:03:38 +0900

    Add active_language to node

--- cookbooks/service/default.rb
+++ cookbooks/service/default.rb
@@ -13,3 +13,24 @@ node[:services][:enabled].each do |name|
     action [:start, :enable]
   end
 end
+
+# isuconの参照実装の一覧を取得する
+result = run_command("systemctl list-unit-files --type=service 'isu*'")
+isucon_services = result.stdout.strip.each_line.filter_map do |line|
+  if line =~ /^(isu.+\..+)\.service/
+    $1
+  end
+end
+
+# active_languageに書かれた言語のみを起動する
+isucon_services.each do |name|
+  if name.end_with?(".#{node[:services][:active_language]}")
+    service name do
+      action [:start, :enable]
+    end
+  else
+    service name do
+      action [:stop, :disable]
+    end
+  end
+end
