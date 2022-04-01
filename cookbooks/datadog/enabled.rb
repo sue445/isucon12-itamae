1e1c9d344baf30b67f46377cec6d29dde63aa0ed
sue445 <sue445@sue445.net>
2022-04-02 00:48:26 +0900

    Migrated to install_datadog_agent_integration

--- cookbooks/datadog/enabled.rb
+++ cookbooks/datadog/enabled.rb
@@ -8,9 +8,3 @@ include_recipe "datadog::install"
 service "datadog-agent" do
   action [:start, :enable]
 end
-
-node[:datadog][:integration].each do |name, version|
-  execute "datadog-agent integration install -t #{name}==#{version} --allow-root" do
-    not_if "datadog-agent integration show #{name} | grep #{version}"
-  end
-end
