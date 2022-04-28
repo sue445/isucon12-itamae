f05d83e6baa30860c40e58535c791b5193f2d6d0
sue445 <sue445@sue445.net>
2022-04-28 23:53:17 +0900

    Fix CI

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -2,7 +2,8 @@
 
 CARGO_BIN = "/home/isucon/.cargo/bin"
 
-execute "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh" do
+# c.f. https://github.com/isucon/isucon11-qualify/blob/a443b242596003515c3037540dd3e48abfa87382/provisioning/ansible/roles/langs.rust/tasks/main.yml
+execute "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y" do
   not_if "#{CARGO_BIN}/rustc --version"
   user "isucon"
 end
