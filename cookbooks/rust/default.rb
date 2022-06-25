55250feb107f3ed6722e61e5a4408e7f9fb8ea6d
sue445 <sue445@sue445.net>
2022-06-25 23:12:27 +0900

    Install curl

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -2,6 +2,12 @@
 
 CARGO_BIN = "/home/isucon/.cargo/bin"
 
+[
+  "curl"
+].each do |name|
+  package name
+end
+
 # c.f. https://github.com/isucon/isucon11-qualify/blob/a443b242596003515c3037540dd3e48abfa87382/provisioning/ansible/roles/langs.rust/tasks/main.yml
 execute "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y" do
   not_if "#{CARGO_BIN}/rustc --version"
