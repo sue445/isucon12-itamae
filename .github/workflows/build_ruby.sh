b434498224c3dfcd33e7bfeb185d8c523723e327
sue445 <sue445@sue445.net>
2022-06-26 00:42:24 +0900

    Check version

--- .github/workflows/build_ruby.sh
+++ .github/workflows/build_ruby.sh
@@ -5,3 +5,6 @@ readonly BUNDLE=$(which bundle)
 
 sudo ${BUNDLE} exec itamae local --node-yaml $NODE_FILE test/cookbooks/default.rb
 sudo COOKBOOK="rust,ruby" ${BUNDLE} exec itamae local --node-yaml $NODE_FILE cookbooks/default.rb
+
+/home/isucon/.cargo/bin/rustc --version
+/home/isucon/local/ruby/versions/${XBUILD_RUBY_VERSION}/bin/ruby --yjit -ve 'p RubyVM::YJIT.enabled?'
